b381759bc9b207982188feb63b5cf366953a3be1
MaristelaP <marispeg@gmail.com>
2023-05-14 16:35:28 -0300

    alterado nome da classe do segundo código e refatorado terceiro codigo

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -2,7 +2,7 @@ public class Calculadora {
     public static void main(String[] args) {
         int x = 10;
         int y = 5;
-        
+
         int somarXeY = x + y;
         System.out.println("A soma é " + somarXeY);
 
--- src/VerificarCondicional.java
+++ src/VerificarCondicional.java
@@ -1,22 +1,31 @@
 public class VerificarCondicional {
-
     public static void main(String[] args) {
-        int numero =32;
+        int a = 7;
+        int b = 30;
+        int c = 3;
+        int d = 3;
+        int e = -1;
+        int f = 100;
 
-        if (numero <= 10) {
-            System.out.println("Este número é menor ou igual a 10!");
-        } else if (numero >= 5) {
-            System.out.println("Este número é maior ou igual a 5!");
-        } else if (numero != 3) {
-            System.out.println("Este número não é igual a 3!");
-        } else if (numero == 8) {
-            System.out.println("Este número é igual a 8!");
-        } else if (numero < 0) {
-            System.out.println("Este número é menor que 0!");
-        } else if (numero > 100) {
-            System.out.println("Este número é maior que 100!");
-        } else {
-            System.out.println("Todas as condições foram atendidas!");
-        }
+            if (a <= 10) {
+                System.out.println("a é menor ou igual a 10!");
+            } else if (b >= 5) {
+                System.out.println("b é maior ou igual a 5!");
+            } else if (c == 3) {
+                System.out.println("c é igual a 3!");
+            } else if (d != 8) {
+                System.out.println("d é Não é igual a 8!");
+            } else if (e < 0) {
+                System.out.println("e é menor que 0!");
+            } else if (f <= 100) {
+                System.out.println("f é menor ou igual a 100!");
+            } else {
+                System.out.println("Todas as condições foram atendidas!");
+            }
+
     }
 }
+
+
+
+
